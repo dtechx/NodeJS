aed9f27b2b00be9e874016e5e89aeb53408066f1
Dushantha Eranga <dushantha@simct.com>
2020-12-05 20:50:19 +0530

    Sample logger

---
+++ logger/app.js
@@ -0,0 +1,11 @@
+
+const EventEmitter = require('events');
+
+const Logger = require('./logger');
+const logger = new Logger();
+
+logger.on('messageLogged', (arg) => {
+    console.log('Listner called', arg);
+})
+
+logger.log('message')
