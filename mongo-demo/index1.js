c3d31ab367bc81b4c7fd787dc95fb80efb797a00
Dushantha Eranga <dushantha@simct.com>
2020-12-08 00:00:24 +0530

    Built in validators and custom validators

--- mongo-demo/index1.js
+++ mongo-demo/index1.js
@@ -1,4 +1,4 @@
-// Mongodb explanation
+// Mongodb CRUD operations with mongoose
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/playground', {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log('Connected to MongoDB...'))
