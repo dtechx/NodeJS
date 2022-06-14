de5b755d5ce460b8919fa71661aae246195ad3f4
Dushantha <dushantha@simct.com>
2022-06-15 00:13:54 +0530

    Using promises

---
+++ async-await/script-promise.js
@@ -0,0 +1,26 @@
+function makeRequest(location) {
+    return new Promise((resolve, reject) => {
+        console.log("Making request to", location);
+        if (location === "Google") {
+            resolve("Google says hi");
+        } else {
+            reject("We can only talk to Google");
+        }
+    })
+}
+
+function processRequest(response) {
+    return new Promise((resolve, reject) => {
+        console.log("Processing response");
+        resolve(`Extra information + ${response} `);
+    })
+}
+
+makeRequest("Google").then(response => {
+    console.log("Response received");
+    return processRequest(response);
+}).then(processedResponse => {
+    console.log(processedResponse);
+}).catch(err => {
+    console.log(err);
+})
